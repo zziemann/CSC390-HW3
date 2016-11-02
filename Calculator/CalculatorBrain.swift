6a0358240310a20170d95dd561bdf91351bc9942
Zach Ziemann <Zach@stevenbnsiphone.reshall>
2016-11-01 23:37:45 -0500

    Initial Setup Commit, for changes in ViewController and CalculatorBrain and Main.storyboard.

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,178 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Zach Ziemann on 9/12/16.
+//  Copyright © 2016 Zach Ziemann. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private var accumulator = 0.0
+    
+    private var internalProgram = [AnyObject]()
+    
+    private var descriptionString = ""
+    private var equalsPrevious = false
+    private var unaryPrevious = false
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+        internalProgram.append(operand)
+    }
+    
+    func setHistory(history: String) {
+        descriptionString = history
+    }
+    
+    func clearHistory() {
+        descriptionString = ""
+        accumulator = 0
+        equalsPrevious = false
+        unaryPrevious = false
+        pending = nil
+        internalProgram.removeAll()
+    }
+    
+    //dictionary is swift thing, generic type
+    //first parameter of dictionary is key, second is value
+    private var operations: Dictionary<String,Operation> = [
+        "Pi" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "ran" : Operation.Constant(Double(arc4random_uniform(10000))/10000),
+        "±" : Operation.UnaryOperation({ -$0 }),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "sin" : Operation.UnaryOperation(sin),
+        "tan" : Operation.UnaryOperation(tan),
+        "log" : Operation.UnaryOperation(log),
+        //closures, in line functions
+        //$0,$1, etc default arguments
+        //the return is now inferred
+        "x²" : Operation.UnaryOperation({ pow($0, 2.0) }),
+        "×" : Operation.BinaryOperation({ $0 * $1 }),
+        "÷" : Operation.BinaryOperation({ $0 / $1 }),
+        "+" : Operation.BinaryOperation({ $0 + $1 }),
+        "-" : Operation.BinaryOperation({ $0 - $1 }),
+        "=" : Operation.Equals
+    ]
+    
+    //enum similar to class, can have methods
+    //contains all operations we know how to do
+    //enum = discrete set of values, can't have inheritance
+    //passed by value
+    private enum Operation {
+        //enums can have associated values
+        case Constant(Double)
+        //function associated value
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        internalProgram.append(symbol)
+        if equalsPrevious == true {
+            descriptionString = ""
+            equalsPrevious = false
+        }
+        //how you look up things in a dictionary
+        //this if fixes the option unwrapping problem
+        if let operation = operations[symbol] {
+            //what we do based on what could happen (every scenario)
+            switch operation {
+                //swift infers that it's operation.Constant
+                
+            case .Constant(let value):
+                descriptionString = descriptionString + String(symbol)
+                accumulator = value
+                
+            case .UnaryOperation(let function):
+                if unaryPrevious == true {
+                    descriptionString = descriptionString + symbol + "(" + String(accumulator) + ")"
+                    unaryPrevious = false
+                }
+                else {
+                    descriptionString = descriptionString + symbol + "(" + String(accumulator) + ")"
+                    unaryPrevious = true
+                }
+                accumulator = function(accumulator)
+                
+            case .BinaryOperation(let function):
+                descriptionString = descriptionString + String(accumulator) + String(symbol)
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+                
+            case .Equals:
+                descriptionString = descriptionString + String(accumulator)
+                executePendingBinaryOperation()
+                equalsPrevious = true
+                
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation()
+    {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    //structs are passed by value, classes are passed by reference
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var description: String {
+        get
+        {
+            return descriptionString
+        }
+    }
+    
+    var isPartialResult: Bool {
+        get {
+            if pending != nil {
+                return true
+            }
+            else {
+                return false
+            }
+        }
+    }
+    //documentation here that our program is a propertylist
+    typealias PropertyList = AnyObject
+    var program: PropertyList {
+        get{
+            //returning a copy, not a point so it's safe
+            return internalProgram
+        }
+        set{
+            clearHistory()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand)
+                    }else if let operation = op as? String {
+                        performOperation(operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    //computed var, and only implementing get the so it's read only
+    var result: Double {
+        get {
+            return accumulator
+            
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -4,22 +4,118 @@
 //
 //  Created by Zach Ziemann on 9/8/16.
 //  Copyright © 2016 Zach Ziemann. All rights reserved.
-//
+
+
+//  Don't do calculations in a controllers
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    //property
+    @IBOutlet private weak var display: UILabel!
+    
+    @IBOutlet private weak var history: UILabel!
+    
+    //helps for when we want longer numbers
+    private var userIsInTheMiddleOfTyping = false
+    
+    private var decimalPressed = false
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction private func clear(sender: UIButton) {
+        displayValue = 0
+        historyValue = ""
+        brain.clearHistory()
+        userIsInTheMiddleOfTyping = false
     }
+    
+    @IBAction func backspace(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            var tempy = display.text!
+            tempy.removeAtIndex(tempy.endIndex.predecessor())
+            displayValue = Double(tempy)!
+            
+        }
+    }
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        //whenever a digit button is pressed, set this var to the text in the button
+        let digit = sender.currentTitle!
 
+        if userIsInTheMiddleOfTyping {
+            //getting text that's in our display box right now and setting it to this var
+            let textCurrentlyInDisplay = display.text!
+            
+            //need to check to see if we've already entered a decimal point
+            if decimalPressed == true && sender.currentTitle! == "." {
+            }
+            else {
+            //new text is what we had before plus our new number
+            display.text = textCurrentlyInDisplay + digit
+            }
+        }
+        else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
+        if sender.currentTitle! == "." {
+            decimalPressed = true
+        }
+    
+    }
+    
+    //computed property, not stored but computed (shown by curly braces)
+    private var displayValue: Double! {
+        get {
+            //this is a double optional cause you don't know if converting to double is legal
+            return Double(display.text!)!
+        }
+        set {
+            //newValue is special keyword, a double based on our defintion
+            let roundingFormatter = NSNumberFormatter()
+            roundingFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+            roundingFormatter.maximumFractionDigits = 6
+            display.text = roundingFormatter.stringFromNumber(newValue)
+        }
+    }
+    
+    private var historyValue: String {
+        get {
+            return history.text!
+        }
+        set {
+            //newValue is special keyword, a double based on our defintion
+            if brain.isPartialResult {
+                history.text = String(newValue + " ...")
+            }
+            else {
+                history.text = String(newValue + " =")
+            }
+        }
+    }
+    
+    
+    //"that big green arrow where the controller talks to the model (brain)"
+    //we initialized the brain, don't forget that
+    private var brain = CalculatorBrain()
+    
+    //this section hooks up our controller to our model, the calculations
+    //when an OP button is pressed, do this and get sender button
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+            decimalPressed = false
+        }
+        if let mathematicalSymbol = sender.currentTitle
+        {
+            brain.performOperation(mathematicalSymbol)
+        }
+        //after math is done, come back to here and set the display
+        displayValue = brain.result
+        historyValue = brain.description
+    }
 
 }
 
